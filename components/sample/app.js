b6d4e6331add580e4fc72634971c4568ee051c8e
Pyae Phyo Kyaw <ppk1996kyaw@gmail.com>
2020-07-07 16:30:07 +0630

    Refining

--- components/sample/app.js
+++ components/sample/app.js
@@ -39,13 +39,13 @@ $(document).ready(function () {
           if (data == "Dispatched" || data == "Tested") {
             return ` 
             <button type="button" class="btn btn-warning btn-sm"> Edit</button>
-            <button type="button" class="btn btn-danger btn-sm"> Delete</button>       
+   
             `;
           }
           return ` 
           <button  type="button" class="btn btn-success btn-sm" onclick=" $(this).hide();dispatchSample(${row.id})"> Dispatch</button>
             <button type="button" class="btn btn-warning btn-sm"> Edit</button>
-            <button type="button" class="btn btn-danger btn-sm"> Delete</button>       
+          
             `;
         },
       },
